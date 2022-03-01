083ceac796bc0544759af4bff154860a7ba4ef8a
zyr <zhongyuanrui@landleaf-tech.com>
2022-03-01 17:48:25 +0800

    webpack打包优化

--- src/app.js
+++ src/app.js
@@ -1,36 +1,10 @@
-import "./styles/index.css";
-
-// import "./test.ts";
-// new Promise((resolve) => {
-//   setTimeout(() => {
-//     console.log(1);
-//     resolve();
-//   });
-// }).then(() => {
-//   console.log(2);
-// });
-// a();
-// async function a() {
-//   setTimeout(() => {
-//     console.log(1);
-//   });
-//   const res = await b();
+import _ from "lodash";
+import moduleA from "./modules/moduleA";
+import moduleB from "./modules/moduleB";
+import moduleC from "./modules/moduleC";
+// import("./modules/moduleB").then((res) => {
 //   console.log(res);
-//   console.log(3);
-// }
-
-// async function b() {
-//   console.log(21);
-//   return 2;
-// }
-
-// import index1 from "./styles/index.css";
-
-function component() {
-  const element = document.createElement("div");
-  element.innerHTML = "Hello webpack";
-  element.setAttribute("class", "div1");
-  return element;
-}
-
-document.body.appendChild(component());
+// });
+// import("./modules/moduleC").then((res) => {
+//   console.log(res);
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,82 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    app: ["./src/app.js", "./src/app2.js"],
-    // app2: "./src/app2.js",
-  },
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "./dist"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          // {
-          //   loader: "style-loader",
-          //   options: {
-          //     injectType: "linkTag",
-          //     insert: "head",
-          //   },
-          // },
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                mode: "local",
-                auto: true,
-                // localIdentName: "[path][name]__[local]--[hash:base64:5]",
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "管理输出",
-      chunks: ["app"],
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
+module.exports = (env, argv) => {
+  const common = {
+    mode: "none",
+    entry: {
+      app: "./src/app.js",
+      // app2: "./src/app2.js",
     },
-  },
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "./dist"),
+      assetModuleFilename: "images/[name][ext][query]",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          use: {
+            loader: "html-loader",
+          },
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+            },
+          ],
+        },
+        {
+          test: /\.png/,
+          type: "asset",
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "app.bundle.css",
+      }),
+      new HtmlWebpackPlugin({
+        title: "管理输出",
+        template: "./src/index.html",
+      }),
+      new CleanWebpackPlugin(),
+    ],
+    optimization: {
+      minimize: true,
+      splitChunks: {
+        name: "vendors",
+        chunks: "all",
+        minSize: 0,
+        cacheGroups: {
+          // vendor: {
+          //   test: /[\\/]node_modules[\\/]/,
+          //   name: "vendors",
+          //   chunks: "all",
+          // },
+        },
+      },
+      // runtimeChunk: "single",
+      runtimeChunk: true,
+    },
+    devServer: {
+      port: 9001,
+      static: "./dist",
+      liveReload: false,
+    },
+  };
+  return common;
 };
